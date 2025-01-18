1eb4ffd853de57c06bd51125d928b15a4cc6b02d
Sohan Shanbhag <sohanshanbhag15@gmail.com>
2025-01-18 21:57:03 +0530

    Initial Commit

---
+++ Backend/src/main/java/com/aura/placement_management_system/PlacementManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.aura.placement_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacementManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacementManagementSystemApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/aura/placement_management_system/PlacementManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aura.placement_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacementManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
